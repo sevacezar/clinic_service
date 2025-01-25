2cc52fbc70ee0b5a383aa8702254a893b914453f
Vsevolod Kryazhev <kryazhev_seva@mail.ru>
2025-01-25 18:07:19 +0500

    patients/adding_doctor_checking_in_router

--- code/patients/router.py
+++ code/patients/router.py
@@ -3,6 +3,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from auth.dependencies import get_current_doctor_user
+from auth.models import User
 from dependencies import get_session
 from patients.dao import PatientsDAO
 from patients.models import Patient
@@ -13,7 +15,10 @@ router: APIRouter = APIRouter(prefix='/patients', tags=['Patients'])
 
 
 @router.get('', response_model=list[PatientPydanticOut])
-async def get_all_patients(session: AsyncSession = Depends(get_session)):
+async def get_all_patients(
+    doctor: User = Depends(get_current_doctor_user),
+    session: AsyncSession = Depends(get_session),
+):
     """Get all patients from DB"""
     patients: list[Patient] = await PatientsDAO.get_all(session=session)
     patients_dicts: list[dict] = [patient.to_dict() for patient in patients]
