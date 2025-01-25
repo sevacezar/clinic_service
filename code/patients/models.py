acbb5c2c2f0ca0d1ca40f026b9d11a7dcd02d1e6
Vsevolod Kryazhev <kryazhev_seva@mail.ru>
2025-01-25 13:47:05 +0500

    patients/creating_orm_models_of_diagnoses_and_assoc_table

--- code/patients/models.py
+++ code/patients/models.py
@@ -2,11 +2,31 @@
 
 from datetime import date, datetime
 
+from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
 from database import Base
+
+
+patient_diagnosis_association = Table(
+    'patient_diagnosis',
+    Base.metadata,
+    Column(
+        'patient_id',
+        Integer,
+        ForeignKey('patients.id', ondelete='CASCADE'), 
+        primary_key=True,
+    ),
+    Column(
+        'diagnosis_id',
+        Integer,
+        ForeignKey('diagnoses.id', ondelete='CASCADE'),
+        primary_key=True
+    ),
+)
+
 
 class Patient(Base):
     __tablename__ = 'patients'
@@ -14,5 +34,26 @@ class Patient(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     date_of_birth: Mapped[date]
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
-    
+
+    diagnoses = relationship(
+        'Diagnosis',
+        secondary=patient_diagnosis_association,
+        back_populates='patients'
+    )
+
+
+class Diagnosis(Base):
+    __tablename__ = 'diagnoses'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+
+    patients = relationship(
+        'Patient',
+        secondary=patient_diagnosis_association,
+        back_populates='diagnoses'
+    )
+
+
+
 
