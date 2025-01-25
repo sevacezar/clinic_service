bf4ceae2766d95a2b27382da5134c126712504f2
Vsevolod Kryazhev <kryazhev_seva@mail.ru>
2025-01-25 16:27:53 +0500

    auth/adding_role_model

--- code/auth/models.py
+++ code/auth/models.py
@@ -1,6 +1,8 @@
 """Module with ORM model of users"""
 
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import relationship
 
 from database import Base
 
@@ -12,9 +14,22 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     password_hashed: Mapped[str]
-    is_doctor: Mapped[bool] = mapped_column(default=False)
+    role_id: Mapped[int] = mapped_column(ForeignKey('roles.id'))
+
+    role = relationship('Role', back_populates='users')
 
     def __repr__(self) -> str:
         return f'<User {self.username}>'
+    
 
+class Role(Base):
+    """Role ORM model."""
+    __tablename__ = 'roles'
 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+
+    users = relationship('User', back_populates='role')
+
+    def __repr__(self) -> str:
+        return f'<Role {self.name}>'
